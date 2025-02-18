bca46b14cb9eabac7d6f0c708c6a72f87a697878
alexdgbs <alexdbc24@gmail.com>
2025-02-18 16:41:16 -0600

    Render 50s

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-axios.defaults.baseURL = "https://server-test-ybsn.onrender.com";
+axios.defaults.baseURL = "https://server-test-oqdo.onrender.com";
 
 
 function Dashboard() {
